f4914969c1730d1760d04f27ce63a6ee726a8709
Himanshu <155740533+san8y@users.noreply.github.com>
2026-02-02 19:57:51 +0530

    Change API base URL to production server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://my-project-m156.onrender.com",
 });
 
 // 🔐 Attach token automatically
