74fd0f1d4762240f63ccabcd57fb40848235573a
Himanshu <155740533+san8y@users.noreply.github.com>
2026-02-22 12:42:57 +0530

    Add new allowed origin for CORS

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",   // 👈 ADD THIS
-  
+  "https://beautycabin-1.onrender.com"
 ];
 
 app.use(
